fe3fa07e1e14fd6f72ec61ed30428f5a063100e5
asudheer501 <44058471+asudheer501@users.noreply.github.com>
2018-10-12 17:05:20 +0530

    d

--- src/main/java/com/sudheer/Create.java
+++ src/main/java/com/sudheer/Create.java
@@ -1,7 +1,12 @@
 package com.sudheer;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -19,11 +24,42 @@ public class Create extends HttpServlet {
 
 protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		// For Tracing
-		String trace = request.getRequestURL()+"|"+request.getParameter("instance")+"|"+request.getParameter("siteId")+"|"+request.getParameter("asset")+"|"+request.getParameter("status");
-		trace t=new trace();
-		t.send(trace);
-		// For Tracing
+	// For Tracing
+	
+	String text = request.getRequestURL()+"|"+request.getParameter("instance")+"|"+request.getParameter("siteId")+"|"+request.getParameter("asset")+"|"+request.getParameter("status");
+	/*
+	trace t=new trace();
+	t.send(trace);
+	*/
+	try {
+		String requestUrl = "https://s2121803030.t.eloqua.com/e/f2?elqFormName=apicheck&elqSiteId=2121803030&paragraphText="+text;
+		URL url = new URL(requestUrl);
+	    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+	    connection.setDoInput(true);
+	    connection.setDoOutput(true);
+	    connection.setRequestMethod("POST");
+	    //connection.connect();
+	    connection.setRequestProperty("Accept", "application/json");
+	    connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+	    //System.out.println("1");
+	    OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream(), "UTF-8");
+		//writer.write(payload);
+		//System.out.println("2");
+		writer.close();
+	    BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+	    //StringBuffer jsonString = new StringBuffer();
+	    //String line;
+	    //while ((line = br.readLine()) != null) {
+	    //        jsonString.append(line);
+	    //}
+	    //br.close();
+	    //System.out.println("Hello 0");
+	    connection.disconnect();
+	} catch (Exception e) {
+	        throw new RuntimeException(e.getMessage());
+	}
+	
+	// For Tracing
 		
 		/*
 		PrintWriter out = response.getWriter(); 
@@ -38,18 +74,49 @@ protected void doGet(HttpServletRequest request, HttpServletResponse response) t
 protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
 	// For Tracing
-	String trace = request.getRequestURL()+"|"+request.getParameter("instance")+"|"+request.getParameter("siteId")+"|"+request.getParameter("asset")+"|"+request.getParameter("status");
+	
+	String text = request.getRequestURL()+"|"+request.getParameter("instance")+"|"+request.getParameter("siteId")+"|"+request.getParameter("asset")+"|"+request.getParameter("status");
+	/*
 	trace t=new trace();
 	t.send(trace);
+	*/
+	try {
+		String requestUrl = "https://s2121803030.t.eloqua.com/e/f2?elqFormName=apicheck&elqSiteId=2121803030&paragraphText="+text;
+		URL url = new URL(requestUrl);
+	    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+	    connection.setDoInput(true);
+	    connection.setDoOutput(true);
+	    connection.setRequestMethod("POST");
+	    //connection.connect();
+	    connection.setRequestProperty("Accept", "application/json");
+	    connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+	    //System.out.println("1");
+	    OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream(), "UTF-8");
+		//writer.write(payload);
+		//System.out.println("2");
+		writer.close();
+	    BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+	    //StringBuffer jsonString = new StringBuffer();
+	    //String line;
+	    //while ((line = br.readLine()) != null) {
+	    //        jsonString.append(line);
+	    //}
+	    //br.close();
+	    //System.out.println("Hello 0");
+	    connection.disconnect();
+	} catch (Exception e) {
+	        throw new RuntimeException(e.getMessage());
+	}
+	
 	// For Tracing
 	
-	/*
+
 	PrintWriter out = response.getWriter(); 
 	response.setContentType("application/json"); 
 	response.setCharacterEncoding("UTF-8");
-	out.print("");
+	out.append("{  \"requiresConfiguration\": false }");
 	out.flush();
-	*/
+	
 	doGet(request, response);
 	response.setStatus(200); 
 	
